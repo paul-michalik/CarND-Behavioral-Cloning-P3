0f3c8a6d3c4b250f88e7431510bb1f7d81f40aa0
Paul Michalik <paul.michalik@outlook.com>
2017-07-24 09:28:45 +0200

    Right camera only - crashes on sharp let turn after the bridge.

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.preprocessing.image import img_to_array, load_img, flip_axis, random_shift
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+from keras.models import load_model
 
 def model_input_layer(model, input_shape, cropping, preprocess_func):
     model.add(Cropping2D(cropping=cropping, input_shape=input_shape))
@@ -107,62 +108,77 @@ def model_nvidia(input_shape, cropping, preprocess_func):
 def preprocess_image(image):
     return (image / 255.0) - 0.5
 
-def import_and_split_csv_data(data_path, split_factor=0.8):
-    data = []
+def import_and_split_csv_data(data_path, steering_corr, train_size):
+    X, y = [], []
     with open(data_path) as file:
         reader = csv.reader(file)
-        for line in reader:
-            data.append(line)
+        next(reader)
+        for center, left, right, steering, throttle, brake, speed in reader:
+            #X += [center, left, right]
+            #y += [float(steering),
+            #      float(steering) + steering_corr, 
+            #      float(steering) - steering_corr]
+            #X += [center]
+            #y += [float(steering)]
+            #X += [left]
+            #y += [float(steering) + steering_corr]
+            X += [right]
+            y += [float(steering) - steering_corr]
+            
+        return train_test_split(X, y, train_size = train_size, random_state = 1)
 
-    training_data, validation_data = train_test_split(data,
-                                                      train_size = split_factor,
-                                                      random_state = 1)
-    return training_data, validation_data
+def extract_sample(X, y, idx):
+    name = './data/IMG/' + X[idx].split('/')[-1]
+    image = img_to_array(load_img(name))
+    angle = y[idx]
+    return image, angle
 
-def batched_sample_generator(samples, batch_size=32):
-    num_samples = len(samples)
+def batched_sample_generator(X, y, batch_size=32):
+    num_samples = len(X)
+    assert(len(X) == len(y))
 
-    print("\nnumber of samples = {}, batch size = {}\n".format(num_samples, batch_size))
+    #print("number of samples = {}, batch size = {}".format(num_samples, batch_size))
      
     while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
+        shuffle(X, y)
         for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
-
-            #filenames = []
             images = []
             angles = []
-            for batch_sample in batch_samples:
-                name = './data/IMG/' + batch_sample[0].split('/')[-1]
-
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                #filenames.append(name)
-                images.append(center_image)
-                angles.append(center_angle)
+            for idx in range(offset, min(offset+batch_size, num_samples)):
+                image, angle = extract_sample(X, y, idx)
+                images.append(image)
+                angles.append(angle) 
 
             X_data = np.array(images)
             y_data = np.array(angles)
+
             yield shuffle(X_data, y_data)
-     
-def train_model(model, batch_size=32, epochs=5, verbose=1):
-    train_data, val_data = import_and_split_csv_data('./data/driving_log.csv')
 
-    print("sizes: training = {},  validation = {}".format(len(train_data), len(val_data)))
 
-    max_train_samples = 1280
-    max_val_samples = int(len(val_data) * max_train_samples/len(train_data))
-    train_data, val_data = train_data[0:max_train_samples], val_data[0:max_val_samples]
+def train_model(model, steering_corr, train_size, batch_size=32, epochs=5, verbose=1):
+    Xt,Xv, yt,yv = import_and_split_csv_data('./data/driving_log.csv',
+                                             steering_corr=steering_corr,
+                                             train_size=train_size)
+    len_train_data = len(Xt)
+    len_val_data = len(Xv)
 
-    train_data_gen, val_data_gen = batched_sample_generator(train_data, batch_size), batched_sample_generator(val_data, batch_size)
+    print("training = {},  validation = {}".format(len_train_data, len_val_data))
 
+    train_data_gen = batched_sample_generator(Xt, yt, batch_size)
+    val_data_gen = batched_sample_generator(Xv, yv, batch_size)
+
+    #for i in range(0, len_train_data, batch_size):
+    #    X, y = next(train_data_gen)
+    #    print("{}/{}: {}, {}".format(i, len_train_data, len(X), len(y)))
+    #
+    #return model, 1
+    
     history = model.fit_generator(train_data_gen, 
-                                  samples_per_epoch = len(train_data),
+                                  samples_per_epoch = len_train_data,
                                   validation_data = val_data_gen,
-                                  nb_val_samples = len(val_data),
+                                  nb_val_samples = len_val_data,
                                   nb_epoch=epochs, 
                                   verbose=verbose)
-
     return model, history
 
 import matplotlib.pyplot as plt
@@ -186,15 +202,11 @@ epochs = 5
 model_99 = model_99linessteering((row, col, depth), cropping, preprocess_image)
 model_99.compile(loss='mse', optimizer="adam")
 model_99.summary()
-model_99, history = train_model(model_99, batch_size, epochs, verbose=1)
+model_99, history = train_model(model_99,
+                                steering_corr=0.,#0.05,
+                                train_size=0.8,
+                                batch_size=batch_size, 
+                                epochs=epochs,
+                                verbose=2)
 model_99.save('model_99.h5')
 
-#row, col, depth = 160, 320, 3
-#cropping = ((55, 25), (0, 0))
-#batch_size = 32
-#epochs = 5
-
-#model_nv = model_nvidia((row, col, depth), cropping, preprocess_image)
-#model_nv.compile(loss='mse', optimizer="adam")
-#model_nv.summary()
-#model_nv, history = train_model(model_nv, batch_size, epochs, verbose=2)
